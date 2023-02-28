45b6c6dd4083f00a26d2cbb85db9c6d79ef2aedd
Neha Kamal <neha.kamal20@gmail.com>
2023-02-28 13:09:54 -0800

    added every picture

---
+++ studio2/script.js
@@ -0,0 +1,26 @@
+(function() {
+
+    'use strict';
+    console.log('reading js');
+    document.querySelector('.open').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('overlay').className = 'showing';
+
+
+    });
+    document.querySelector('.close').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('overlay').className = 'hidden';
+
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            document.getElementById('overlay').className = 'hidden';
+        }
+    });
+
+
+    // add script here
+
+})();
