faa0f405bb77c6db4a9748591cc95ed46278da85
Neha Kamal <neha.kamal20@gmail.com>
2023-03-02 01:22:59 -0800

    new every picture iteration

--- studio2/script.js
+++ studio2/script.js
@@ -7,16 +7,23 @@
         document.getElementById('overlay').className = 'showing';
 
 
+
+
+
     });
     document.querySelector('.close').addEventListener('click', function(event) {
         event.preventDefault();
         document.getElementById('overlay').className = 'hidden';
+
+
+
 
     });
 
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Escape') {
             document.getElementById('overlay').className = 'hidden';
+
         }
     });
 
