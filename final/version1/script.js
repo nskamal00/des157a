50f6da0152741e06bf894a7d9291da83ee96a39d
Neha Kamal <neha.kamal20@gmail.com>
2023-03-14 20:48:52 -0700

    version 1 uploaded

---
+++ final/version1/script.js
@@ -0,0 +1,37 @@
+(function() {
+
+    'use strict';
+
+    const openBtns = document.querySelectorAll('.open');
+    console.log(openBtns);
+    const closeBtns = document.querySelectorAll('.close');
+
+    for (const eachBtn of openBtns) {
+        eachBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            const thisBtn = this.id;
+            console.log(thisBtn);
+            if (document.getElementById(`ol-${thisBtn}`).classList.contains('overlay-wide')) {
+                document.getElementById(`ol-${thisBtn}`).className = `overlay-wide showing`;
+            } else {
+                document.getElementById(`ol-${thisBtn}`).className = `overlay-tall showing`;
+            }
+
+
+        });
+    }
+
+    for (const eachBtn of closeBtns) {
+        eachBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            document.querySelector('.showing').className = 'overlay hidden';
+        });
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            document.querySelector('.showing').className = 'overlay hidden';
+        }
+    });
+
+})();
